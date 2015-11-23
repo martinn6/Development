4adc29874f1bd3e2fa9ea31ef9cc60bad3c94c2b
nickdmartin <nickdmartin@outlook.com>
2015-11-22 17:24:58 -0800

    dgjf

--- development.js
+++ development.js
@@ -74,7 +74,7 @@ app.post('/todo',function(req,res){
 	var reqWeather = new XMLHttpRequest();
 	reqWeather.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&APPID=' + apiKey, true);
 
-	reqWeather.addEventListener('load',bind.function()
+	reqWeather.addEventListener('load',function()
 		{
 			var temp2 = "";
 			if(reqWeather.status >= 200 && reqWeather.status < 400)
