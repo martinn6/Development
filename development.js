95227151689d5349bd651be2060fc35ff2f7c14f
nickdmartin <nickdmartin@outlook.com>
2015-11-22 16:16:01 -0800

    test

--- development.js
+++ development.js
@@ -108,11 +108,7 @@ app.post('/todo',function(req,res){
 				})
 			}
 
-			context.name = req.session.name;
-			context.toDoCount = req.session.toDo.length;
-			context.toDo = req.session.toDo;
-			console.log(context.toDo);
-			res.render('todolist',context);
+		
 			
 			
 		});
@@ -122,7 +118,11 @@ app.post('/todo',function(req,res){
 
 
   }
-
+			context.name = req.session.name;
+			context.toDoCount = req.session.toDo.length;
+			context.toDo = req.session.toDo;
+			console.log(context.toDo);
+			res.render('todolist',context);
   
 });
 
